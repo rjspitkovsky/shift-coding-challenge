7f76f18db1c6528262788cb99bad82a76230d0ee
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-07-30 23:48:33 -0400

    stubbed out timer incrementing by 1 second

--- src/timer.js
+++ src/timer.js
@@ -9,33 +9,43 @@ class Timer extends React.Component {
     }
   }
 
+
   startTimer = (event) => {
     event.preventDefault()
-    document.getElementById("timer").append(this.state.timer)
+
     if (this.state.status === "off") {
     this.setState({
-      status: "on",
-      timer: setInterval(() => this.setState({
-        timer: this.state.timer + 1
-      }), 1000)
+      status: "click to start timer"
     })
   }
-    else if (this.state.status === "on") {
+    else if (this.state.status === "click to start timer") {
       this.setState({
-        status: "paused",
-        timer: this.state.timer
+        status: "on",
+        timer: setInterval(() => this.setState({
+          timer: ++this.state.timer
+        }), 1000)
       })
     }
+    // else if (this.state.status === "on") {
+    //   this.setState({
+    //     status: "paused",
+    //     timer: this.state.timer
+    //   })
+    // }
+
     }
+
 
 
   render() {
   return (
     <div>
+    <p>{this.state.status}</p>
     <button onClick={event => this.startTimer(event)}> This is a timer app </button>
     <div>
-      <h3 id="timer"></h3>
-      <p>{this.state.status}</p>
+      <div id="timer">
+      {(this.state.timer !== 0) ? <p>{this.state.timer}</p> : null}
+      </div>
       <ul>
 
       </ul>
