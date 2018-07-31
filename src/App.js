9a5da27aa5734a3ccdb6f4485f1870ea6d48f9c0
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-07-30 22:02:42 -0400

    set up timer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Timer from './timer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hello, world </h1>
-      
+        <Timer />
       </div>
     );
   }
---
+++ src/timer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Timer extends React.Component {
+  render() {
+  return (
+    <h2> This is a timer app </h2>
+  )
+}
+}
+
+export default Timer
