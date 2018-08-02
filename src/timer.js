696c8be7552d3379dcb04d738488b47f13d189a4
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-01 22:25:43 -0400

    deletes subsequent split times after being clicked

--- src/timer.js
+++ src/timer.js
@@ -23,7 +23,7 @@ class Timer extends React.Component {
 
   startTimer = (event) => {
     event.preventDefault()
-    const clickedSeconds = []
+
     if (this.state.status === "off") {
 
       this.setState({
@@ -32,10 +32,10 @@ class Timer extends React.Component {
       this.changeTimer()
     }
     else if (this.state.status === "on") {
-        clickedSeconds.push(this.state.timer.seconds)
+        // clickedSeconds.push(this.state.timer.seconds)
         // const secondSplit = clickedSeconds.map((second) => <li key={second.toString()}>{second}</li>)
         const li = document.createElement("li")
-        li.className = "white"
+        // li.className = "white"
         // const button = document.createElement("button")
         // const liWithButton = li.appendChild(button)
         li.innerHTML = this.state.timer.seconds
@@ -80,14 +80,27 @@ class Timer extends React.Component {
       Array.prototype.forEach.call(lis, li => {
 
             li.addEventListener('click', function() {
-              this.className = "red"
+              this.className = "clicked"
+              // if (li.className !== "clicked" && li.innerHTML > this.innerHTML) {
+              //   document.getElementById("splits").removeElement("li")
+              // }
+
+              for (let i = 0; i < lis.length; i++) {
+                const li = lis[i]
+                if (li.className !== "clicked" && Number(li.innerHTML) > Number(this.innerHTML)) {
+
+                  document.getElementById("splits").removeChild(li)
+                }
+                }
+
+
             //   clearInterval(this.changeTimer())
             //   this.setState({
             //   status: "paused",
             //   timer: {seconds: li.innerHTML}
             // })
 
-            console.log(`${li.innerHTML}`)
+
           })
         })
 
