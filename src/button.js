f523dd61efe26584b90bd1256b23efc44e7828f1
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-06 01:36:30 -0400

    removed comments

--- src/button.js
+++ src/button.js
@@ -1,26 +1,6 @@
 import React from 'react'
 
 class Button extends React.Component {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     job: null
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   if (this.props.status === "off") {
-  //     this.setState({
-  //       job: "startTimer"
-  //     })
-  //   }
-  //   else if (this.props.status === "on"){
-  //     this.setState({
-  //       job: "collectSeconds"
-  //     })
-  //   }
-  // }
-
 
   manageTimer = (event) => {
     event.preventDefault()
--- src/unorderedList.js
+++ src/unorderedList.js
@@ -9,62 +9,14 @@ class UnorderedList extends React.Component {
     var splitTime = Number(event.target.innerText)
     var lis = document.getElementsByTagName("li")
     Array.prototype.map.call(lis, li => {
-
       for (let i = 0; i < lis.length; i++) {
         const li = lis[i]
-
       if (li.className !== "clicked" && splitTime < Number(li.innerText)) {
-        // debugger
           document.getElementById("splits").removeChild(li)
       }
     }
   })
-
     this.props.sendStatusToTimer()
-
-  //          const li = document.createElement("li")
-  //         const p = document.getElementById("secondsTimer")
-  //         li.innerHTML = Number(p.innerHTML)
-  //         document.getElementById("splits").append(li)
-  //         const lis = document.getElementsByTagName("LI")
-  //
-  //       Array.prototype.map.call(lis, li => {
-  //
-  //             li.addEventListener('click', function() {
-  //
-  //               const p = document.getElementById("secondsTimer")
-  //               p.innerHTML = this.innerHTML
-  //               this.className = "clicked"
-  //
-  //               for (let i = 0; i < lis.length; i++) {
-  //                    const li = lis[i]
-  //                 if (li.className !== "clicked" && Number(li.innerHTML) > Number(this.innerHTML)) {
-  //
-  //                   document.getElementById("splits").removeChild(li)
-  //                 }
-  //                 }
-  //           })
-  //         })
-  //       }
-
-
-    // debugger
-    // var lis = document.getElementsByTagName("li")
-    // Array.from(lis).forEach(function(li) {
-    //   if (li.className !== "clicked" && li.innerText > splitTime) {
-    //     document.getElementById("splits").removeElement(li)
-    //   }
-    // })
-    // const lis = document.getElementsByTagName("LI")
-    // debugger
-    // Array.prototype.forEach.call(lis, li => {
-    //   for (let i = 0; i < lis.length; i++) {
-    //     const li = lis[i]
-    //     if (li.className !== "clicked" && Number(li.innerHTML) > Number(event.target.innerHTML)) {
-    //       document.getElementById("splits").removeElement(li)
-    //     }
-    //   }
-    // })
   }
 
   displayListItems = () => {
@@ -78,9 +30,7 @@ class UnorderedList extends React.Component {
 
   render() {
     return (
-
       <ul id="splits">{this.displayListItems()}</ul>
-
     )
   }
 }
