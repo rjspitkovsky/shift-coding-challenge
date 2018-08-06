fc99757cb70fe50e3f3bb951f72911ffeddeea90
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-06 16:13:59 -0400

    removed comments improved syntax

--- src/timer.js
+++ src/timer.js
@@ -5,7 +5,6 @@ class Timer extends React.Component {
   constructor() {
     super()
     this.state = {
-      timer: {seconds:0},
       status: 'off'
     }
   }
@@ -13,87 +12,74 @@ class Timer extends React.Component {
 
   startTimer = (event) => {
     event.preventDefault()
-    // start timer until the first click
-    let timesClicked = 0
-    timesClicked++
-
     if (this.state.status === "off") {
-
       this.setState({
         status: "on"
       })
+
     const interval = setInterval(function() {
       const p = document.getElementById("secondsTimer")
+        if (document.getElementsByClassName("clicked").length === 0)
+          p.innerHTML = Number(p.innerHTML) + 1
+        if (document.getElementsByClassName("clicked").length > 0)
+          clearInterval(interval)
+      }, 1000)
+    }
+    if (document.getElementsByClassName("clicked").length > 0 && this.state.status === "on") {
+      this.setState({
+        status: "off"
+      })
 
-      if (document.getElementsByClassName("clicked").length === 0)
-      p.innerHTML = Number(p.innerHTML) + 1
-      if (document.getElementsByClassName("clicked").length > 0)
-      clearInterval(interval)
-    }, 1000)
-  }
-  if (document.getElementsByClassName("clicked").length > 0 && this.state.status === "on") {
-
-    this.setState({
-      status: "off"
-    })
-    // this restarts the interval if the timer is clicked after a split reset
     const newInterval = setInterval(function() {
       const p = document.getElementById("secondsTimer")
       const clickedSplits = document.getElementsByClassName("clicked").length
-      if (document.getElementsByClassName("clicked").length === clickedSplits)
-      p.innerHTML = Number(p.innerHTML) + 1
-
-      if (document.getElementsByClassName("clicked").length > clickedSplits)
-      clearInterval(newInterval)
-      // p.innerHTML = Number(p.innerHTML)
-    }, 1000)
-  }
+        if (document.getElementsByClassName("clicked").length === clickedSplits)
+          p.innerHTML = Number(p.innerHTML) + 1
+        if (document.getElementsByClassName("clicked").length > clickedSplits)
+          clearInterval(newInterval)
+        }, 1000)
+    }
     if ((this.state.status === "on" || this.state.status === "off") && Number(document.getElementById("secondsTimer").innerHTML) !== 0) {
-      // This should work no matter how many splits have been clicked
-        const li = document.createElement("li")
-        const p = document.getElementById("secondsTimer")
-        li.innerHTML = Number(p.innerHTML)
-        document.getElementById("splits").append(li)
-        const lis = document.getElementsByTagName("LI")
+
+      const li = document.createElement("li")
+      const p = document.getElementById("secondsTimer")
+      li.innerHTML = Number(p.innerHTML)
+      document.getElementById("splits").append(li)
+      const lis = document.getElementsByTagName("LI")
 
       Array.prototype.map.call(lis, li => {
-
-            li.addEventListener('click', function() {
-
-              const p = document.getElementById("secondsTimer")
-              p.innerHTML = this.innerHTML
-              this.className = "clicked"
-
-              for (let i = 0; i < lis.length; i++) {
-                const li = lis[i]
-                if (li.className !== "clicked" && Number(li.innerHTML) > Number(this.innerHTML)) {
-
+          li.addEventListener('click', function() {
+            const p = document.getElementById("secondsTimer")
+            p.innerHTML = this.innerHTML
+            this.className = "clicked"
+            for (let i = 0; i < lis.length; i++) {
+              const li = lis[i]
+              if (li.className !== "clicked" && Number(li.innerHTML) > Number(this.innerHTML)) {
                   document.getElementById("splits").removeChild(li)
-                }
-                }
+              }
+            }
           })
-        })
-      }
-
+      })
     }
+  }
 
   render() {
   return (
     <div>
     {/*<p>{this.state.status}</p>*/}
-    <button onClick={event => this.startTimer(event)}> This is a timer app </button>
+      <button onClick={event => this.startTimer(event)}> This is a timer app </button>
 
-    <div>
-      <div id="timer">
-      <p id="secondsTimer">0</p>
-      </div>
-      <ul id="splits">
+      <div>
+        <div id="timer">
+          <p id="secondsTimer">0</p>
+        </div>
 
-      </ul>
+        <ul id="splits"></ul>
+
       </div>
-      </div>
-  )
-}
+    </div>
+    )
+  }
 }
 
 export default Timer
